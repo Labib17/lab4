d34e00e2454334e83405c3bd187e3cc8312b31c0
Labib17 <42694482+Labib17@users.noreply.github.com>
2021-01-15 16:17:04 +0200

    Create dodavanyya.go

---
+++ commands/dodavanyya.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"strconv"
+	"fmt"
+	"github.com/"
+)
+
+type Add struct {
+	Arg1, Arg2 int
+}
+
+type Print struct {
+	Arg string
+}
+
+func (a *Add) Execute(loop engine.Handler) {
+	sum := a.Arg1 + a.Arg2
+	loop.Post(&Print{Arg: strconv.Itoa(sum)})
+}
+
+func (p *Print) Execute(loop engine.Handler) {
+	fmt.Println(p.Arg)
+}
